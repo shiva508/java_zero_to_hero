433f30ab5fb98e19cb25610386ed554523c73ad5
shiva508 <dasarishiva1@gmail.com>
2023-05-04 22:05:53 +0530

    Basic

---
+++ arrays/src/main/java/com/pool/ClassObjectsforArrays.java
@@ -0,0 +1,26 @@
+package com.pool;
+
+import org.apache.commons.codec.Charsets;
+
+public class ClassObjectsforArrays {
+    public static void main(String[] args) {
+        int[] intArray = new int[2];
+        byte[] byteArray = new byte[3];
+        short[] shortArray = new short[4];
+        String[] stringArray = new String[5];
+        System.out.println(intArray.getClass());
+        System.out.println(intArray.getClass().getSuperclass());
+        System.out.println(byteArray.getClass());
+        System.out.println(byteArray.getClass().getSuperclass());
+        System.out.println(shortArray.getClass());
+        System.out.println(shortArray.getClass().getSuperclass());
+        System.out.println(stringArray.getClass());
+        System.out.println(stringArray.getClass().getSuperclass());
+
+        String name="Shiva";
+        byte[] nameByte=name.getBytes(Charsets.UTF_8);
+        System.out.println(nameByte);
+        String ddd=new String(nameByte, Charsets.UTF_8);
+        System.out.println(ddd );
+    }
+}
---
+++ arrays/src/main/java/com/pool/clone/CloningOfArrays.java
@@ -0,0 +1,29 @@
+package com.pool.clone;
+
+public class CloningOfArrays {
+    public static void main(String[] args) {
+        singleDimentional();
+        multiDimentional();
+    }
+
+    private static void multiDimentional() {
+        int[][] multiArray = { { 1, 2 }, { 3, 4 } };
+        int[][] cloneArray = multiArray.clone();
+        System.out.println(multiArray == cloneArray);
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                System.out.print(cloneArray[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+
+    private static void singleDimentional() {
+        int[] intArray = { 1, 3, 4 };
+        int[] cloneArray = intArray.clone();
+        System.out.println(cloneArray == intArray);
+        for (int i = 0; i < cloneArray.length; i++) {
+            System.out.print(cloneArray[i]);
+        }
+    }
+}
